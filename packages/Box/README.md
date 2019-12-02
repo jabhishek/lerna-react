2d310344bdbd1a45abf9ca980d90cbade48b442b
Abhishek Jain <abhi2000@gmail.com>
2019-12-02 23:31:52 +0000

    [Box]: added box component

---
+++ packages/Box/README.md
@@ -0,0 +1,11 @@
+# `@avj/box`
+
+> TODO: description
+
+## Usage
+
+```
+const box = require('@avj/box');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/Box/lib/index.d.ts
@@ -0,0 +1,2 @@
+import * as React from 'react';
+export declare const Box: import("@emotion/styled-base").StyledComponent<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, any, object>;

0c391587211312155eaaee3a757fc313893f0c75
Abhishek Jain <abhi2000@gmail.com>
2019-12-11 21:38:02 +0000

    add types to box component

--- packages/Box/lib/index.d.ts
+++ packages/Box/lib/index.d.ts
@@ -1,2 +1,15 @@
-import * as React from 'react';
-export declare const Box: import("@emotion/styled-base").StyledComponent<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, any, object>;
+import { BackgroundProps, BorderProps, ColorProps, FlexboxProps, GridProps, LayoutProps, PositionProps, ShadowProps, SpaceProps, TypographyProps, OpacityProps, OverflowProps } from 'styled-system';
+import * as React from "react";
+import { ButtonHTMLAttributes } from "react";
+declare type StyledSystemProps = LayoutProps & ColorProps & SpaceProps & BorderProps & BackgroundProps & PositionProps & FlexboxProps & ShadowProps & GridProps & OpacityProps & OverflowProps & TypographyProps;
+interface ButtonHTMLProps extends React.DOMAttributes<any>, ButtonHTMLAttributes<any> {
+    as: 'button';
+}
+interface AllHTMLProps extends React.DOMAttributes<any> {
+    as: string;
+}
+declare type HTMLProps = AllHTMLProps | ButtonHTMLProps;
+declare type BoxHTMLProps = React.RefAttributes<any> & HTMLProps;
+declare type BoxProps = BoxHTMLProps & StyledSystemProps;
+export declare const Box: React.FunctionComponent<BoxProps>;
+export {};
--- packages/Box/src/index.tsx
+++ packages/Box/src/index.tsx
@@ -1,20 +1,51 @@
-import * as React from 'react';
 import styled from "@emotion/styled";
 import shouldForwardProp from '@styled-system/should-forward-prop';
 import {
-    background,
-    border,
-    color,
-    flexbox,
-    grid,
-    layout,
-    position,
-    shadow,
-    space,
-    typography,
+    background, BackgroundProps,
+    border, BorderProps,
+    color, ColorProps,
+    flexbox, FlexboxProps,
+    grid, GridProps,
+    layout, LayoutProps,
+    position, PositionProps,
+    shadow, ShadowProps,
+    space, SpaceProps,
+    typography, TypographyProps,
+    opacity, OpacityProps,
+    overflow, OverflowProps,
 } from 'styled-system';
+import * as React from "react";
+import {ButtonHTMLAttributes} from "react";
 
-export const Box = styled('div', {shouldForwardProp})(
+type StyledSystemProps = LayoutProps &
+    ColorProps &
+    SpaceProps &
+    BorderProps &
+    BackgroundProps &
+    PositionProps &
+    FlexboxProps &
+    ShadowProps &
+    GridProps &
+    OpacityProps &
+    OverflowProps &
+    TypographyProps;
+
+interface ButtonHTMLProps extends React.DOMAttributes<any>, ButtonHTMLAttributes<any> {
+    as: 'button';
+}
+
+interface AllHTMLProps extends React.DOMAttributes<any> {
+    as: string;
+}
+
+type HTMLProps = AllHTMLProps | ButtonHTMLProps;
+
+type BoxHTMLProps = React.RefAttributes<any> & HTMLProps;
+
+type BoxProps = BoxHTMLProps &
+    StyledSystemProps;
+
+export const Box: React.FunctionComponent<BoxProps> = styled('div', {shouldForwardProp})(
     background,
     border,
     color,
@@ -25,4 +56,6 @@ export const Box = styled('div', {shouldForwardProp})(
     shadow,
     space,
     typography,
+    opacity,
+    overflow,
 );
